dd6ef25795f87f52e6bde71b9bee86df93efce55
Shashank Patil <shashank.patil82@gmail.com>
2024-02-06 20:59:20 +0530

    Username info added in leapyear script

--- leapyear.sh
+++ leapyear.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #This script will check year leapyear
 
+read -p "Enter Name: " name
 read -p "Enter year: " year
+
+echo "Hello $name !!!"
 
 if (($year % 4 == 0)); then
    echo "$year is the leap year"
